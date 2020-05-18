234251ab2df0b65c075a36e371fd677891341ffb
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-05-19 00:57:19 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+$stringOne = 'my email is ';
+$stringTwo= 'moaz.ahmed132@gmail.com';
+
+//echo $stringOne . $stringTwo;
+$name ='moaz';
+
+//echo "my name is  $name";
+
+//echo "moaz said \"whaaa\"";
+//echo 'moaz said "whaaaa"';
+//echo $name[3];
+//echo strlen($name)
+//echo strtoupper($name);
+//echo str_replace('a', 'aa', $name);
+//$radius = 25;
+$pi = 3.14;
+//echo $pi * $radius**2;
+
+//$age = 20;
+//$age *= 10;
+//echo $age;
+
+echo ceil($pi);
+
+
+ ?>
+
+ <!DOCTYPE html>
+ <html>
+ <head>
+ 	<title>my first php </title>
+ </head>
+ <body>
+ <h1>User Profile Page</h1>
+
+
+ </body>
+ </html>
