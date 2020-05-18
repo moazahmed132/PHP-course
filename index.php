7cd7574578be7f638d7593d98e40c241d3135942
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-05-19 01:30:54 +0200

    index and associative arrays

--- index.php
+++ index.php
@@ -1,27 +1,59 @@
 <?php 
-$stringOne = 'my email is ';
-$stringTwo= 'moaz.ahmed132@gmail.com';
+ 
+// indexed arrays
 
-//echo $stringOne . $stringTwo;
-$name ='moaz';
+		// indexed arrays of strings 
+		$peopleOne = ['moaz', 'nada', 'eman'];
+		//echo $peopleOne[1];
 
-//echo "my name is  $name";
+		// indexed arrays of strings in another way
+		$peopleTwo = array('khalaf', 'shehata');
+		//echo $peopleTwo[0];
 
-//echo "moaz said \"whaaa\"";
-//echo 'moaz said "whaaaa"';
-//echo $name[3];
-//echo strlen($name)
-//echo strtoupper($name);
-//echo str_replace('a', 'aa', $name);
-//$radius = 25;
-$pi = 3.14;
-//echo $pi * $radius**2;
+		// array of numbers and how to print it
+		$ages = [20, 30, 40, 50];
+		//print_r($ages);
 
-//$age = 20;
-//$age *= 10;
-//echo $age;
+		// over write an element of array 
+		$ages[1] = 35;
+		//print_r($ages);
 
-echo ceil($pi);
+		// add element to the end of the array 
+		$ages[] = 60;
+		//print_r($ages);
+
+		// add element to the end of the array in another way 
+		array_push($ages, 70);
+		//print_r($ages);
+
+		// count the array elements
+		//echo count($ages);
+
+		// merge arrays
+
+		$peopleMerged = array_merge($peopleOne, $peopleTwo);
+		//print_r($peopleMerged);
+
+// associative arrays (key & value pairs)
+
+		//create an associative array
+		$ninjasOne = ['moaz' => 'black', 'nada' => 'orange', 'khaled' => 'brown' ];
+		//print_r($ninjasOne);
+
+		//create an associative array in another way
+		$ninjasTwo = array('khalaf' => 'red', 'shehata' => 'blue');
+		//print_r($ninjasTwo);
+
+		// over write an element
+		$ninjasTwo['khalaf'] = 'pink' ;
+		//print_r($ninjasTwo);
+
+		// count the associative array elements
+		//echo count($ninjasTwo)
+
+		// merge two associative arrays 
+		$ninjasMerged = array_merge($ninjasOne, $ninjasTwo);
+		print_r($ninjasMerged);
 
 
  ?>
@@ -29,11 +61,9 @@ echo ceil($pi);
  <!DOCTYPE html>
  <html>
  <head>
- 	<title>my first php </title>
+ 	<title>PHP Tutorials </title>
  </head>
  <body>
- <h1>User Profile Page</h1>
-
 
  </body>
  </html>
