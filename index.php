363744d0751fd8822961fdc1f7f48ce9ffb8ca41
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-05-20 17:21:08 +0200

    add header and footer to the project

--- index.php
+++ index.php
@@ -1,23 +1,8 @@
-<?php 
-
-// include and require
-
-include('ninjas.php'); // can written like include 'ninjas.php'
-require('ninjas.php'); // can written like require 'ninjas.php'
-
-// the difference between them is the include function will render the rest of the code of the attached file crashed but require will not display any thing if there was a problem
-
-
-echo 'end of php';
-	
- ?>
 
  <!DOCTYPE html>
  <html>
- <head>
- 	<title>PHP Tutorials </title>
- </head>
- <body>
-	<?php include 'content.php'; ?>
- </body>
+ 
+	<?php include 'templates/header.php'; ?>
+	<?php include 'templates/footer.php'; ?>
+ 
  </html>
