2feffb32bf74337bef6410fa8247be0153f120e1
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-05-20 04:22:55 +0200

    more examples about functions

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php 
 
 // functions
+	// we can use default value
+	function sayHello($name = 'moaz', $time = 'morning'){
+		echo "good $time $name";
+	}
 
+	//sayHello('ahmed', 'after noon');
 
+	function formatProduct($product){
+		//echo "{$product['name']} costs {$product['price']} to buy";
+		return "{$product['name']} costs {$product['price']} to buy";
+	}
+	/* $formatted = formatProduct(['name' => 'gold star', 'price' => '20']);
+	echo $formatted; */
  ?>
 
  <!DOCTYPE html>
